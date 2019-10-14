064cfe99f583e1fb60759ea5d016ba4ab1f2c7ac
vitorserrano <vitorcirinoserrano@gmail.com>
2019-10-14 13:03:27 -0300

    CodeCast

---
+++ READEME.md
@@ -0,0 +1,5 @@
+# CodeCast
+
+## Para Funcionar: 
+ - Utilize o LiveServer do Visual Studio Code, para startar a aplicação.
+
---
+++ codecast/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Eternal Youth",
+    artist: "RŮDE",
+    cover: "eternal-youth.jpg",
+    file: "eternal-youth.mp3"    
+  },
+  {
+    title: "Shangri-La",
+    artist: "SPEECHLESS",
+    cover: "shangri-la.jpg",
+    file: "shangri-la.mp3"    
+  },
+  {
+    title:"Mellow Skies",
+    artist: "Hakaisu",
+    cover: "mellow-skies.jpg",
+    file: "mellow-skies.mp3"
+  }
+];
